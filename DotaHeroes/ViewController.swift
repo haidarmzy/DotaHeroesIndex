2e4af8893296652e09f870c1d2c4a48a5e91b892
Haidar Ammarafif Ramzy <haidara@xl.co.id>
2020-10-22 06:47:11 +0700

    Initial Commit

---
+++ DotaHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DotaHeroes
+//
+//  Created by Haidar Ammarafif Ramzy on 22/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
